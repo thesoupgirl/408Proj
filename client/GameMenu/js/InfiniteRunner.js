18263e993f9008541cdf858bcc75acae007d2bbd
Lonswaya <alonswa@purdue.edu>
2018-02-12 22:06:54 -0500

    Snacks and animated doggies

--- client/GameMenu/js/InfiniteRunner.js
+++ client/GameMenu/js/InfiniteRunner.js
@@ -21,6 +21,7 @@ function GameMode() {
       //console.log(gameObjects[i]);
       self.deleteOnRestart[i].destroy = true;
     }
+    self.scoreCounterGameObject.score = 0;
     self.animalGameObject.physics.drag = 0;
     self.animalGameObject.position = new Vector3(100, 400, 50);
 
@@ -110,6 +111,7 @@ function GameMode() {
       if (self.gameState == 1) {
         self.gameState = 2;
         animalSelect = 0;
+        animalGameObject.animalSelect = 0;
         self.restart();
       }
     }
@@ -132,6 +134,7 @@ function GameMode() {
       if (self.gameState == 1) {
         self.gameState = 2;
         animalSelect = 1;
+        animalGameObject.animalSelect = 1;
         self.restart();
       }
     }
@@ -142,23 +145,35 @@ function GameMode() {
 
 
     //  dog/cat object
-    self.baseSpeed = 100;
+    self.baseSpeed = 200;
     var animalWidth = 80;
     var animalHeight = 50;
-    var animalGameObject = new GameObject(new Vector3(0, (canvas.height / 2) - (animalHeight/2), 1), new Vector2(animalWidth, animalHeight), "Animal Object", true);
+    var animalGameObject = new GameObject(new Vector3(0, -500, 1), new Vector2(animalWidth, animalHeight), "Animal Object", true);
     animalGameObject.clickThrough = true;
     animalGameObject.physics.gravityScale = 0;
     animalGameObject.physics.drag = 0; // Since we directly set velocity
+    animalGameObject.animalSelect = 0;
+    animalGameObject.lastTime = time;
+    animalGameObject.animFrames = 80; // New animation frame every 100 ms
+    animalGameObject.animIndex = 1;
     animalGameObject.draw = function () {
-      if (self.gameState == 2 || self.gameState == 3) {
-        context.fillStyle = "#FFFFFF";
-        context.fillRect(this.position.x,this.position.y,this.bounds.x,this.bounds.y);
-        context.fillStyle = "#000000";
-        context.strokeRect(this.position.x, this.position.y, this.bounds.x, this.bounds.y);
-      }
+      // We still render the image, just out of bounds
+      // That way we don't have gif resource loading issues
+        if (time - this.lastTime > this.animFrames && self.gameState != 3) {
+          if (self.gameState <= 1 && this.animIndex == (this.animalSelect == 0 ? 7 : 5)) {
+            this.animalSelect++; // Iterate through all animals to load each sprite in the beginning5
+          }
+          this.animIndex = (this.animIndex + 1) % ((this.animalSelect == 0 ? 7 : 5) + 1);
+          this.lastTime = time;
+        }
+        var img=new Image();
+        var animalType = this.animalSelect == 0 ? "dog" : "cat";
+        img.src = "../src/game1/" + animalType + "_run/" + animalType + (this.animIndex+1) + ".png";
+        context.drawImage(img,this.position.x - 10,this.position.y - 10, this.bounds.x + 20, this.bounds.y + 20);
+
     }
     animalGameObject.onTick = function () {
-      if (self.gameState == 2) {
+      if (self.gameState == 2 || self.gameState == 3) {
         // Idk do the bounds checking thing
         var depth = parseInt(((this.position.y - 390)/55).toFixed(0)) + 1;
         this.position.z = parseInt(depth); // So we have the image constnatly moving in the foreground/background
@@ -200,6 +215,7 @@ function GameMode() {
 
       } else {
         this.physics.velocity = new Vector3(0,0,0);
+        this.position.x = -500;
       }
     }
     this.checkKeyDown = function (e) {
@@ -232,6 +248,7 @@ function GameMode() {
     var scoreWidth = 200;
     var scoreHeight = 50;
     var scoreCounterGameObject = new GameObject(scorePosition, new Vector2(scoreWidth, scoreHeight), "Score Counter", false);
+    self.scoreCounterGameObject = scoreCounterGameObject;
     scoreCounterGameObject.score = 0;
     scoreCounterGameObject.draw = function () {
       if (self.gameState == 2 || self.gameState == 3) {
@@ -240,15 +257,16 @@ function GameMode() {
         context.fillStyle = "#000000";
         context.strokeRect(this.position.x, this.position.y, this.bounds.x, this.bounds.y);
         context.font = "30px Arial";
-        context.fillText("Score: " + this.score,this.position.x + 20,this.position.y + (this.bounds.y/2) + 10);
+        context.fillText("Score: " + parseInt(this.score),this.position.x + 20,this.position.y + (this.bounds.y/2) + 10);
       }
     }
     scoreCounterGameObject.onTick = function () {
       if (self.gameState != 2) {
         this.startTime = time;
       } else {
-        this.score = (time - this.startTime)/10;
+        this.score += (time - this.lastTime)/50;
       }
+      this.lastTime = time;
     }
     self.animalGameObject = animalGameObject;
     addGameObject(animalGameObject);
@@ -300,11 +318,20 @@ function GameMode() {
 
   }
   // Nextspawn is for ensuring that the next item isn't colliding
+  // Rate is, of course, rate. The higher, the less spawned.
   self.nextSpawnNoCollide = 0;
-  self.nextSpawnObstacle = 0;
   self.noCollideRate = 5;
+
+  self.nextSpawnObstacle = 0;
   self.obstacleRate = 5;
-  self.backgroundVelocity = -80;
+
+  self.nextSpawnTreat = 0;
+  self.treatRate = 15;
+
+  self.nextSpawnPlayable = 0;
+  self.playableRate = 2;
+
+  self.backgroundVelocity = -120;
   this.obstacles = [];
   this.tick = function() {
     // Called every frame update
@@ -348,7 +375,7 @@ function GameMode() {
             break;
           case 2:
           // PLANT
-            decorY = 300;
+            decorY = 150;
             decorDimensions = new Vector2(63, 92);
             decorPath = "../src/game1/decor3.png";
             break;
@@ -380,7 +407,6 @@ function GameMode() {
 
 
       // Obstacles
-      var obstacleRate = 8;
 
       if (time >= this.nextSpawnObstacle && Math.random() <= (0.2/(timeInc * self.obstacleRate))) {
         var obstacleType = (Math.random() * 5).toFixed(0);
@@ -466,13 +492,136 @@ function GameMode() {
             var top = this.position.y  + this.bounds.y;
             var bottom = top - 55;
             if ((self.animalGameObject.position.y + self.animalGameObject.bounds.y >= bottom && self.animalGameObject.position.y + self.animalGameObject.bounds.y <= top)) {
-              console.log("PHAT COLLISION: bottom: " + bottom + ", top: " + top + ", pos: " + (self.animalGameObject.position.y + self.animalGameObject.bounds.y));
-              self.gameOver();
+              //console.log("PHAT COLLISION: bottom: " + bottom + ", top: " + top + ", pos: " + (self.animalGameObject.position.y + self.animalGameObject.bounds.y));
+              if (self.gameState == 2) self.gameOver();
             }
           }
         }
         addGameObject(obstacleGameObject);
         this.deleteOnRestart.push(obstacleGameObject);
+      }
+
+      // Treats
+      var treatScore = 500;
+
+      if (time >= this.nextSpawnTreat && Math.random() <= (0.2/(timeInc * self.treatRate))) {
+        var treatType = (Math.random() * 5).toFixed(0);
+        // console.log("Spawning an treat of type " + treatType);
+        // Defaults
+        var treatDimensions = new Vector2(0, 0);
+        var treatPath = "";
+
+        switch (parseInt(treatType)) {
+          case 0:
+          // Treat
+            treatDimensions = new Vector2(50, 37);
+            treatPath = "../src/game1/treat1.png";
+            break;
+          case 1:
+          // Treat
+            treatDimensions = new Vector2(50, 35);
+            treatPath = "../src/game1/treat2.png";
+            break;
+          case 2:
+          // Treat
+            treatDimensions = new Vector2(50, 34);
+            treatPath = "../src/game1/treat3.png";
+            break;
+          case 3:
+          // Treat
+            treatDimensions = new Vector2(50, 50);
+            treatPath = "../src/game1/treat4.png";
+            break;
+          case 5:
+          // Treat
+            treatDimensions = new Vector2(50, 22);
+            treatPath = "../src/game1/treat5.png";
+            break;
+          case 6:
+          // Treat?
+            treatDimensions = new Vector2(50, 43);
+            treatPath = "../src/game1/treat6.png";
+
+            break;
+
+          default:
+
+        }
+        var chosenLane = parseInt((Math.random() * 3).toFixed(0)); //up to 4 options
+        var chosenLaneOffset = treatDimensions.y;
+        switch (parseInt(chosenLane)) {
+          case 0:
+            chosenLaneOffset = 420 - chosenLaneOffset;
+            break;
+          case 1:
+            chosenLaneOffset = 475 - chosenLaneOffset;
+            break;
+          case 2:
+            chosenLaneOffset = 530 - chosenLaneOffset;
+            break;
+          case 3:
+            chosenLaneOffset = 585 - chosenLaneOffset;
+            break;
+          default:
+
+        }
+        //console.log("spawning in lane " + chosenLane)
+
+        this.nextSpawntreat = time + (-1000 * treatDimensions.x / self.backgroundVelocity);
+        var treatGameObject = new GameObject(new Vector3(canvas.width,chosenLaneOffset,parseInt(chosenLane)), treatDimensions, "Treat " + time, true);
+        //console.log("Spawned game object at " + chosenLane);
+        treatGameObject.physics.drag = 0;
+        treatGameObject.physics.gravityScale = 0;
+        treatGameObject.image = treatPath;
+        treatGameObject.chosenLane = chosenLane;
+        treatGameObject.draw = function () {
+          var img=new Image();
+          img.src = this.image;
+          context.drawImage(img,this.position.x,this.position.y, this.bounds.x, this.bounds.y);
+        }
+        treatGameObject.movingTicking = movingTicking;
+        treatGameObject.onTick = function () {
+          this.movingTicking();
+          // Search for collision for animal
+          if ((self.animalGameObject.position.x >= this.position.x && self.animalGameObject.position.x <= this.position.x + this.bounds.x) || (self.animalGameObject.position.x +  self.animalGameObject.bounds.x >= this.position.x && self.animalGameObject.position.x + self.animalGameObject.bounds.x <= this.position.x + this.bounds.x)) {
+            // Lined up horizontally, now vertically?
+            var top = this.position.y  + this.bounds.y;
+            var bottom = top - 55;
+            if ((self.animalGameObject.position.y >= this.position.y && self.animalGameObject.position.y <= this.position.y + this.bounds.y) || (self.animalGameObject.position.y +  self.animalGameObject.bounds.y >= this.position.y && self.animalGameObject.position.y + self.animalGameObject.bounds.y <= this.position.y + this.bounds.y)) {
+              //console.log("PHAT COLLISION: bottom: " + bottom + ", top: " + top + ", pos: " + (self.animalGameObject.position.y + self.animalGameObject.bounds.y));
+              self.scoreCounterGameObject.score += treatScore;
+              // Floaty up score
+              var scoreGameObject = new GameObject(this.position, new Vector2(200, 100), "Score thingy", true);
+              scoreGameObject.physics.drag = 3;
+              scoreGameObject.physics.gravityScale = 0;
+              scoreGameObject.movingTicking = movingTicking;
+              scoreGameObject.score = treatScore;
+              scoreGameObject.spawnTime = time;
+              scoreGameObject.scoreLifetime = 3000;
+              scoreGameObject.scoreFontSize = 20;
+              scoreGameObject.onTick = function () {
+                this.movingTicking();
+                if (time - this.spawnTime > this.scoreLifetime) {
+                  deleteGameObject(this);
+                }
+              }
+              scoreGameObject.draw = function () {
+                var fontFactor = Math.sqrt(this.scoreFontSize * ((-((time - this.spawnTime)/this.scoreLifetime)) + 1));
+                console.log(fontFactor);
+                if (fontFactor < 1 || isNaN(fontFactor)) fontFactor = 0; //Workaround
+                var fontSize = 10 * fontFactor;
+                context.font = (fontSize) + "px Arial";
+                context.fillText("+" + this.score, this.position.x, this.position.y);
+              }
+              scoreGameObject.physics.addForce(new Vector3(0, -100, 0));
+              addGameObject(scoreGameObject);
+              self.deleteOnRestart.push(scoreGameObject);
+              deleteGameObject(this);
+            }
+          }
+        }
+        addGameObject(treatGameObject);
+        this.deleteOnRestart.push(treatGameObject);
       }
 
     }
