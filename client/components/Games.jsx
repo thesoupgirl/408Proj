2dbf5fe98b9ac8c58a16df8a1ede1eca664a95a3
Lisa Campbell <campb215@purdue.edu>
2018-02-21 13:34:42 -0500

    almost works

--- client/components/Games.jsx
+++ client/components/Games.jsx
@@ -1,11 +1,21 @@
-import htmlContent from '../GameMenu/html/game_menu.html';
 import React from 'react';
+import Iframe from 'react-iframe'
 
 const Games = React.createClass({
     render() {
-        return (
-            <div dangerouslySetInnerHTML={ {__html: htmlContent} } />
-        );
-    }
+
+    return (
+      <div className='container'>
+        <Iframe url="https://epsteingames.herokuapp.com/"
+            position="absolute"
+            width="100%"
+            id="myId"
+            className="myClassname"
+            height="100%"
+            styles={{height: "200px"}}
+            allowFullScreen/>
+      </div>
+    )
+  }
 });
 export default Games;
