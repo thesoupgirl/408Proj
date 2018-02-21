5773ba88823bdbd06ee74e509e6f24434bd6065a
Jeff Hughes <jeff@jhughes.xyz>
2018-02-21 13:19:14 -0500

    Calendar stuffs

--- EpsteinAndroid/app/src/main/java/xyz/jhughes/epsteinandroid/activities/CalendarActivity.java
+++ EpsteinAndroid/app/src/main/java/xyz/jhughes/epsteinandroid/activities/CalendarActivity.java
@@ -1,8 +1,10 @@
 package xyz.jhughes.epsteinandroid.activities;
 
+import android.app.ProgressDialog;
 import android.graphics.RectF;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.alamkanak.weekview.MonthLoader;
 import com.alamkanak.weekview.WeekView;
@@ -14,12 +16,22 @@ import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 import xyz.jhughes.epsteinandroid.R;
+import xyz.jhughes.epsteinandroid.models.Events.Events;
+import xyz.jhughes.epsteinandroid.networking.EpsteinApiHelper;
+import xyz.jhughes.epsteinandroid.utilities.SharedPrefsHelper;
 
 public class CalendarActivity extends AppCompatActivity implements WeekView.EventClickListener, MonthLoader.MonthChangeListener{
 
     @BindView(R.id.weekView)
     WeekView weekView;
+
+    ProgressDialog dialog;
+
+    Events events;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +43,27 @@ public class CalendarActivity extends AppCompatActivity implements WeekView.Even
         weekView.setOnEventClickListener(this);
         weekView.setMonthChangeListener(this);
 
+        dialog = ProgressDialog.show(this, "", "Loading your calendar. Please wait...", true);
+        dialog.show();
+
+        EpsteinApiHelper.getInstance().getEvents(
+                SharedPrefsHelper.getSharedPrefs(this).getString("email", null),
+                SharedPrefsHelper.getSharedPrefs(this).getString("idToken", null)
+        ).enqueue(new Callback<Events>() {
+            @Override
+            public void onResponse(Call<Events> call, Response<Events> response) {
+                System.out.println(response.body());
+                dialog.cancel();
+
+                weekView.notifyDatasetChanged();
+            }
+
+            @Override
+            public void onFailure(Call<Events> call, Throwable t) {
+                t.printStackTrace();
+                dialog.cancel();
+            }
+        });
     }
 
     protected String getEventTitle(Calendar time) {
@@ -39,8 +72,16 @@ public class CalendarActivity extends AppCompatActivity implements WeekView.Even
 
     @Override
     public List<? extends WeekViewEvent> onMonthChange(int newYear, int newMonth) {
+
+
         // Populate the week view with some events.
         List<WeekViewEvent> events = new ArrayList<WeekViewEvent>();
+
+        if (this.events == null) {
+            Toast.makeText(this, "Looks like you don't have anything! Stress free!", Toast.LENGTH_LONG).show();
+
+            return events;
+        }
 
         Calendar startTime = Calendar.getInstance();
         startTime.set(Calendar.HOUR_OF_DAY, 3);
