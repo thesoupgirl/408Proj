bb713fd89d8181eb5f023cffcda10401417dd222
Winnie <winnielin@Winnies-MacBook-Pro.local>
2018-03-07 19:39:19 -0500

    add images to homepage

--- client/components/HomePage.jsx
+++ client/components/HomePage.jsx
@@ -7,10 +7,15 @@ import {
   MenuItem,
   Nav,
   Navbar,
-  NavDropdown
+  NavDropdown,
+  Image,
+  Grid,
+  Row, 
+  Col
 } from 'react-bootstrap'
 import { map } from 'lodash'
 import SweetAlert from 'react-bootstrap-sweetalert'
+import flower from '../image/flower.png'
 
 
 class HomePage extends React.Component {
@@ -31,12 +36,13 @@ class HomePage extends React.Component {
       <div className='container'>
         <Jumbotron>
           <p>Welcome to Epstein.</p>
-         
-
-
-        
 
         </Jumbotron>
+ 
+  
+    <img  width={900} height={600} alt="900x600"  src={require('../image/flower.png')} /> 
+      
+
         
       </div>
       )
