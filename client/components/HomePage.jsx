80292086d763b46e986ac3d911df1e18da43da0a
Deepthi Naidu <naidud@pal-nat186-123-182.itap.purdue.edu>
2018-03-10 21:30:55 -0500

    added styling

--- client/components/HomePage.jsx
+++ client/components/HomePage.jsx
@@ -17,7 +17,6 @@ import { map } from 'lodash'
 import SweetAlert from 'react-bootstrap-sweetalert'
 import flower from '../image/flower.png'
 
-
 class HomePage extends React.Component {
   
   constructor(props) {
@@ -36,11 +35,12 @@ class HomePage extends React.Component {
       <div className='container'>
         <Jumbotron>
           <p>Welcome to Epstein. We are so glad you decided to use our smart calendar to help you manage and balance your day to day activities and alleviate your stress from the chaos of life! </p>
-
         </Jumbotron>
- 
 
-<img width={600} height={900} src={require('../image/flower.png')} style={{position: "relative", top: 100, left: 250}}/>
+
+<img width={600} height={900} src={require('../image/flower.png')} style={{position: "relative", border: '50px solid pink', top: 100, left: 250}}/>
+
+
         
       </div>
       )
