0f0c05caf684b08be38849c0cd2afb3a37644713
Deepthi Naidu <naidud@pal-nat186-123-182.itap.purdue.edu>
2018-03-20 23:02:36 -0400

    Unit Testing For User Story #1 and #5 Completed

---
+++ client/__tests__/SettingsPage-test.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+import UserPage from '../components/UserPage';
+import { has } from 'lodash'
+import moment from 'moment'
+import SettingsPage from '../components/SettingsPage';
+
+// Note: test renderer must be required after react-native.
+import renderer from 'react-test-renderer';
+
+//tests to see if SettingsPage exists
+test('Check to see if SettingsPage exists', () => {
+
+  	const settings = shallow(
+	  	<SettingsPage />
+    );
+	expect(settings.contains('SettingsPage'));
+});
