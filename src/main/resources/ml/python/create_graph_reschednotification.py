713f6492bc22241c6595f353e89129ac501dfddb
Lonswaya <alonswa@purdue.edu>
2018-03-27 16:57:26 -0400

    Update resched graph

--- src/main/resources/ml/python/create_graph_reschednotification.py
+++ src/main/resources/ml/python/create_graph_reschednotification.py
@@ -2,30 +2,20 @@ from __future__ import print_function
 
 import tensorflow as tf
 
-pS = tf.placeholder(tf.int32, name='pS') # Combined stress for Sunday
-pM = tf.placeholder(tf.int32, name='pM') # Combined stress for Monday
-pT = tf.placeholder(tf.int32, name='pT') # Combined stress for Tuesday
-pW = tf.placeholder(tf.int32, name='pW') # Combined stress for Wednesday
-pTh = tf.placeholder(tf.int32, name='pTh') # Combined stress for Thursday
-pF = tf.placeholder(tf.int32, name='pF') # Combined stress for Friday
-pSa = tf.placeholder(tf.int32, name='pSa') # Combined stress for Saturday
+pStress = tf.placeholder(tf.int32, name='pStress') # Combined stress for All days
 
 # eventStress = tf.placeholder(tf.int32, name="eventStress") # Stress of the event in question
 
 y_ = tf.placeholder(tf.float64, name='target') # Time from the event in question to the predicted reschedule time
 
 
-iS = tf.Variable(1., name='iS') # Influence of Sunday
-iM = tf.Variable(1., name='iM') # Influence of Monday
-iT = tf.Variable(1., name='iT') # Influence of Tuesday
-iW = tf.Variable(1., name='iW') # Influence of Wednesday
-iTh = tf.Variable(1., name='iTh') # Influence of Thursday
-iF = tf.Variable(1., name='iF') # Influence of Friday
-iSa = tf.Variable(1., name='iSa') # Influence of Saturday
+iStress = tf.Variable(1., name='iStress') # Influence of stress
 
 # y = (eventStress / (pS * iS)) + (pS * iS) + (eventStress / (pM * tf.clip_by_value(iM, 0, 10))) + (pM * tf.clip_by_value(iM, 0, 10)) +
 
-y = tf.cast((tf.cast(pS, tf.float32) * tf.clip_by_value(iS, 0, 10)) + (tf.cast(pS, tf.float32) * tf.clip_by_value(iS, 0, 10)) + (tf.cast(pM, tf.float32) * tf.clip_by_value(iM, 0, 10)) + (tf.cast(pM, tf.float32) * tf.clip_by_value(iM, 0, 10)) + (tf.cast(pT, tf.float32) * tf.clip_by_value(iT, 0, 10)) + (tf.cast(pT, tf.float32) * tf.clip_by_value(iT, 0, 10)) + (tf.cast(pW, tf.float32) * tf.clip_by_value(iW, 0, 10)) + (tf.cast(pW, tf.float32) * tf.clip_by_value(iW, 0, 10)) + (tf.cast(pTh, tf.float32) * tf.clip_by_value(iTh, 0, 10)) + (tf.cast(pTh, tf.float32) * tf.clip_by_value(iTh, 0, 10)) + (tf.cast(pF, tf.float32) * tf.clip_by_value(iF, 0, 10)) + (tf.cast(pF, tf.float32) * tf.clip_by_value(iF, 0, 10)) + (tf.cast(pSa, tf.float32) * tf.clip_by_value(iSa, 0, 10)) + (tf.cast(pSa, tf.float32) * tf.clip_by_value(iSa, 0, 10)), tf.float64)
+# y = tf.cast((tf.cast(pS, tf.float32) * tf.clip_by_value(iS, 0, 10)) + (tf.cast(pS, tf.float32) * tf.clip_by_value(iS, 0, 10)) + (tf.cast(pM, tf.float32) * tf.clip_by_value(iM, 0, 10)) + (tf.cast(pM, tf.float32) * tf.clip_by_value(iM, 0, 10)) + (tf.cast(pT, tf.float32) * tf.clip_by_value(iT, 0, 10)) + (tf.cast(pT, tf.float32) * tf.clip_by_value(iT, 0, 10)) + (tf.cast(pW, tf.float32) * tf.clip_by_value(iW, 0, 10)) + (tf.cast(pW, tf.float32) * tf.clip_by_value(iW, 0, 10)) + (tf.cast(pTh, tf.float32) * tf.clip_by_value(iTh, 0, 10)) + (tf.cast(pTh, tf.float32) * tf.clip_by_value(iTh, 0, 10)) + (tf.cast(pF, tf.float32) * tf.clip_by_value(iF, 0, 10)) + (tf.cast(pF, tf.float32) * tf.clip_by_value(iF, 0, 10)) + (tf.cast(pSa, tf.float32) * tf.clip_by_value(iSa, 0, 10)) + (tf.cast(pSa, tf.float32) * tf.clip_by_value(iSa, 0, 10)), tf.float64)
+
+y = tf.cast(tf.cast(pStress, tf.float32) * iStress, tf.float64);
 
 y = tf.identity(y, name='output')
 
@@ -40,26 +30,14 @@ init = tf.global_variables_initializer()
 saver_def = tf.train.Saver().as_saver_def()
 
 print('Operation to initialize variables:       ', init.name)
-print('Tensors to feed value pS:                ', pS.name)
-print('Tensors to feed value pM:                ', pM.name)
-print('Tensors to feed value pT:                ', pT.name)
-print('Tensors to feed value pW:                ', pW.name)
-print('Tensors to feed value pTh:               ', pTh.name)
-print('Tensors to feed value pF:                ', pF.name)
-print('Tensors to feed value pSa:               ', pSa.name)
+print('Tensors to feed value pStress:                ', pStress.name)
 print('Tensor to feed as training targets:      ', y_.name)
 print('Tensor to fetch as prediction:           ', y.name)
 print('Operation to train one step:             ', train_op.name)
 print('Tensor to be fed for checkpoint filename:', saver_def.filename_tensor_name)
 print('Operation to save a checkpoint:          ', saver_def.save_tensor_name)
 print('Operation to restore a checkpoint:       ', saver_def.restore_op_name)
-print('Tensor to read value of iS               ', iS.value().name)
-print('Tensor to read value of iM               ', iM.value().name)
-print('Tensor to read value of iT               ', iT.value().name)
-print('Tensor to read value of iW               ', iW.value().name)
-print('Tensor to read value of iTh              ', iTh.value().name)
-print('Tensor to read value of iF               ', iF.value().name)
-print('Tensor to read value of iSa              ', iSa.value().name)
+print('Tensor to read value of iS               ', iStress.value().name)
 
 with open('../graphs/graph_rescheduling_notification.pb', 'wb') as f:
     f.write(tf.get_default_graph().as_graph_def().SerializeToString())
