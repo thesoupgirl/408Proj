6971d888014b4ca494f74e2f9c213b520e3d16b2
Deepthi Naidu <naidud@pal-nat186-123-182.itap.purdue.edu>
2018-04-23 00:47:27 -0400

    unit testing added

--- client/__tests__/ApplicationGuide-test.js
+++ client/__tests__/ApplicationGuide-test.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import {shallow} from 'enzyme';
-import ImportPage from '../components/ImportPage';
+import ApplicationGuide from '../components/ApplicationGuide';
+import { has } from 'lodash'
+import moment from 'moment'
+
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
--- client/__tests__/DestressPage-test.js
+++ client/__tests__/DestressPage-test.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import {shallow} from 'enzyme';
-import ImportPage from '../components/ImportPage';
+import DestressPage from '../components/DestressPage';
+import { has } from 'lodash'
+import moment from 'moment'
+
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
-//tests to see if Application Guide exists
+//tests to see if Destress Page exists
 test('Check to see if Destress Page exists', () => {
 
   	const home = shallow(
